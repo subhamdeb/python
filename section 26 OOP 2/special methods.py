f0c090b693665523a078151776376d9a446243ae
Subham Deb <subham.github.1253@gmail.com>
2020-08-04 12:40:40 +0530

    4 july

--- section 26 OOP 2/special methods.py
+++ section 26 OOP 2/special methods.py
@@ -7,8 +7,13 @@ class Human:
     return f"Human name {self.first} {self.last}"
   def __len__(self):
     return self.age
+  def __add__(self,other):
+    if isinstance(other, Human):
+      return Human(first = "NewBorn", last = self.last,age=0)
+    return "second pertion is not an Inheritance of Human"
 
 j = Human("Jenny", "Larsen",47)
-
+k = Human("kenny","anderson",45)
 print(j)
 print(len(j))
+print(k + j)
