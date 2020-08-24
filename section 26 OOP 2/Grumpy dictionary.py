85300eb951459e1c713a85972e90aa2126d58605
Subham Deb <subham.github.1253@gmail.com>
2020-08-24 23:32:27 +0530

    24 aug

--- section 26 OOP 2/Grumpy dictionary.py
+++ section 26 OOP 2/Grumpy dictionary.py
@@ -2,7 +2,20 @@ class GrumpyDict(dict):
   def __repr__(self):
     print("NONE OF YOUR BUSINESS")
     return super().__repr__()
+  def __missing__(self,key):
+    print(f"ypun want {key}? well it aint here!")
+  def __setitem__(self,key,value):
+    super().__setitem__(key,value)
+
+
 
 data = GrumpyDict({"first" : "Tom","animal" : "cat"})
 
 print(data)
+
+data["animal"] = "Dog"
+print(data)
+
+
+data2 = dict({"name" : "Subham"})
+print(data2)
